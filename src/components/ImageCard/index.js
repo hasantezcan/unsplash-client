576cf1e54a97f057f619e8a819071af7d59796f9
hasantezcan <hasantezcan77@gmail.com>
2021-04-07 17:36:00 +0300

    Fix download image button issue

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -1,13 +1,39 @@
 import style from "./style.module.css";
 import cn from "classnames";
 
+import { useSearch } from "../../Context/Search";
 import { BiDownArrowAlt, BiPlus } from "react-icons/bi";
 
 const ImageCard = ({ image }) => {
-	
+	const { queryInput, selectedCollection } = useSearch();
+
+	function fetchImage(url) {
+		return fetch(url)
+			.then((response) => {
+				return response.blob();
+			})
+			.then((blob) => {
+				return URL.createObjectURL(blob);
+			});
+	}
+
+	async function downloadImage(image, id) {
+		const a = document.createElement("a");
+		a.href = await fetchImage(image);
+		a.download =
+			(queryInput ? queryInput + "-" : "") +
+			(selectedCollection.id ? selectedCollection.value + "-" : "") +
+			id +
+			".png";
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	}
+
 	return (
 		<div className={style.imageCard}>
-			{/* {  Image Button } */}
+			<p>{selectedCollection.value}</p>
+			{/* {  Image Card } */}
 			<a
 				className={style.image}
 				href={image.links.html}
@@ -17,16 +43,14 @@ const ImageCard = ({ image }) => {
 				<img src={image.urls.small} alt={image.alt_description} />
 			</a>
 			{/* {  Download Button } */}
-			<a
-				href={image.urls.raw}
-				download
-				target="_blank"
-				rel="noopener noreferrer"
+			<span
+				onClick={() => {
+					downloadImage(image.urls.raw, image.id);
+				}}
+				className={cn(style.button, style.downloadButton)}
 			>
-				<span className={cn(style.button, style.downloadButton)}>
-					<BiDownArrowAlt className={style.downloadIcon} />
-				</span>
-			</a>
+				<BiDownArrowAlt className={style.downloadIcon} />
+			</span>
 			{/* {  Collect Button } */}
 			<a href={image.urls.raw} target="_blank" rel="noopener noreferrer">
 				<span className={cn(style.button, style.collectButton)}>
